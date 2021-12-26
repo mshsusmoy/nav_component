e9254c749136d58fe8d7783123dc723815a99c43
mshsusmoy <mshsusmoy@gmail.com>
2021-12-26 17:31:49 +0600

    Android Fragment Navigation Component

---
+++ NavComponent/app/src/main/java/com/susmoy/navcomponent/FirstFragment.java
@@ -0,0 +1,38 @@
+package com.susmoy.navcomponent;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController nav = Navigation.findNavController(view);
+
+        Button btn = (Button) view.findViewById(R.id.textFirstFragment);
+        btn.setOnClickListener(v -> {
+            nav.navigate(R.id.action_firstFragment_to_secondFragment);
+        });
+    }
+}
---
+++ NavComponent/app/src/main/java/com/susmoy/navcomponent/SecondFragment.java
@@ -0,0 +1,38 @@
+package com.susmoy.navcomponent;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class SecondFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController nav = Navigation.findNavController(view);
+
+        Button btn = (Button) view.findViewById(R.id.textSecondFragment);
+        btn.setOnClickListener(v -> {
+            nav.navigate(R.id.action_secondFragment_to_firstFragment);
+        });
+    }
+}
